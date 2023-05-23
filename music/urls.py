bf1d23346883e5abbc4ae3e4fd6eff585d608067
Steve Lehew <120685948+SLehew@users.noreply.github.com>
2023-05-23 16:02:50 -0400

    need to complete edit album function

--- music/urls.py
+++ music/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.album_list, name='album_list'),
     path('album/<int:pk>', views.album_detail, name='album-detail'),
-    path('album/new', views.create_album, name="album-new")
+    path('album/new', views.create_album, name="album-new"),
+    path('album/<int:pk>/delete', views.delete_album, name='delete-album'),
+    path('album/<int:pk>/edit', views.edit_album, name='edit-album'),
 ]
--- music/views.py
+++ music/views.py
@@ -13,6 +13,30 @@ def album_detail(request, pk):
     return render(request, 'music/album_detail.html', {'album': album})
 
 
+def delete_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    album.delete()
+    return redirect('album_list')
+
+
+def edit_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == "POST":
+        form = AlbumForm(request.POST, instance=album)
+        if form.is_valid():
+            album = form.save(commit=False)
+            album.save()
+            return redirect('album-detail', pk=album.pk)
+    else:
+        form = AlbumForm(instance=album)
+    return render(request, 'edit-album'), {'form': form}
+
+
 def create_album(request):
-    form = AlbumForm()
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(request.POST)
+        form.save()
+        return redirect('album_list')
     return render(request, 'music/new_album.html', {'form': form})
